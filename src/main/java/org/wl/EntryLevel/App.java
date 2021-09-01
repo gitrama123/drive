caa6502a4eccf55abb6015d87df09b051c6afb50
ramakrishna <ram@gmail.com>
2021-09-01 21:38:29 +0530

    Modified App.java again

--- src/main/java/org/wl/EntryLevel/App.java
+++ src/main/java/org/wl/EntryLevel/App.java
@@ -12,6 +12,7 @@ public class App
     {
     	
         System.out.println(WordUtils.capitalize("hello world!" ));
+        System.out.println(WordUtils.capitalizewe are feature branch" ));
         
     }
 }
